ea8c4cc4e6111bd7c67817e1fda5485939a25462
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-19 15:58:05 -0700

    added function stater code for building a deck function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 console.log('test');
 
-// deck of cards 
-
+// deck of cards constants
 const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'J', 'Q', 'K'];
 
+// build master deck function 
+function buildDeck() {
+
+}
