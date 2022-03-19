c5c27666b521ed5a48df27862da5617866fd2f9f
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-19 15:19:21 -0700

    added constant variables for deck of cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 console.log('test');
+
+// deck of cards 
+
+const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'J', 'Q', 'K'];
+
