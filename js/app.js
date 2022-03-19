8cb8ef64be1e4a4899a8eb6bfbc647bbb724795b
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-19 16:39:48 -0700

    added a new object that iterates inside each suit and rank array and creates a face and value property

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-console.log('test');
-
 // deck of cards constants
 const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'J', 'Q', 'K'];
 
+const masterDeck = buildDeck();
+
 // build master deck function 
 function buildDeck() {
     const deck = [];
-}
+    suits.forEach(suit => {
+        ranks.forEach(rank => {
+            deck.push({
+                face: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck
+};
