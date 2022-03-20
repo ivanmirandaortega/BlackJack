995e1d5087a77a3a2f29d17bed1e8d5244b15afc
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-19 17:05:23 -0700

    initiated a function that shuffles the deck

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 // deck of cards constants
-const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'J', 'Q', 'K'];
 
+// master deck variable that contains the buildDeck objects 
 const masterDeck = buildDeck();
 
-// build master deck function 
+// master deck function 
 function buildDeck() {
     const deck = [];
     suits.forEach(suit => {
         ranks.forEach(rank => {
             deck.push({
-                face: `${suit}${rank}`,
-                value: Number(rank) || (rank === 'A' ? 11 : 10)
+                face: `${suit} ${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10) 
             });
         });
     });
     return deck
 };
+
+// shuffle deck funciton 
+function shuffleDeck() {
+    
+}
